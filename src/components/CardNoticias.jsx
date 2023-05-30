e0136cc1c68e86813821b46012b6155b5f3284fd
Mauro Rosales <maurorosales65@gmail.com>
2023-05-30 10:28:37 -0300

    inicio proyect Helvecia+ 2023

---
+++ src/components/CardNoticias.jsx
@@ -0,0 +1,7 @@
+
+
+export default function CardNoticias() {
+  return (
+    <div>CardNoticias</div>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import "./scss/styles.scss"
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
